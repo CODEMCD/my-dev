78a57d78e6ed4510c1c64f25ca30060c578a8a6e
CODEMCD <psbum1609@gmail.com>
2020-05-27 16:18:23 +0900

    project init

---
+++ src/test/java/com/codemcd/mydev/MyDevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codemcd.mydev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDevApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
