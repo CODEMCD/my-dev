622fa08a9c580b9fef7d53683a1d3befd17b1895
CODEMCD <psbum1609@gmail.com>
2020-05-28 10:55:06 +0900

    feat: LinkRepository 구현
    - 기본적인 CRUD 테스트 추가

---
+++ src/test/java/com/codemcd/mydev/linkbucket/domain/LinkRepositoryTest.java
@@ -0,0 +1,59 @@
+package com.codemcd.mydev.linkbucket.domain;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+public class LinkRepositoryTest {
+
+    @Autowired
+    private LinkRepository linkRepository;
+
+    @Test
+    @DisplayName("링크 엔티티 CRUD 작업이 데이터베이스에서 정상적으로 동작하는지 확인한다.")
+    void crud() {
+        // Create
+        Link link = Link.builder()
+                .url("www.github.com/codemcd")
+                .title("my github")
+                .description("This is...")
+                .image("image").build();
+
+        Link savedLink = linkRepository.save(link);
+
+        assertThat(savedLink).isEqualTo(link);
+
+        // Read
+        Link foundLink = linkRepository.findById(link.getId()).orElseThrow(IllegalArgumentException::new);
+
+        assertThat(foundLink).isEqualTo(link);
+
+        // Update
+        Link updateLink = Link.builder()
+                .url("updatedURL")
+                .title("updatedTitle")
+                .description("Updated...")
+                .image("updatedImage").build();
+
+        link.update(updateLink);
+
+        Link updatedLink = linkRepository.findById(link.getId()).orElseThrow(IllegalArgumentException::new);
+
+        assertThat(updatedLink.getUrl()).isEqualTo(updateLink.getUrl());
+        assertThat(updatedLink.getTitle()).isEqualTo(updateLink.getTitle());
+        assertThat(updatedLink.getDescription()).isEqualTo(updateLink.getDescription());
+        assertThat(updatedLink.getImage()).isEqualTo(updateLink.getImage());
+
+        // Delete
+        linkRepository.delete(link);
+
+        assertThat(linkRepository.findById(link.getId())).isEmpty();
+    }
+}
